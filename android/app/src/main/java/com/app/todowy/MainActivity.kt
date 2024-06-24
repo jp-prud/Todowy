4b9dd68beed13481c808240a2a352169592742a9
João Pedro Prudêncio <113187913+jp-prudencio@users.noreply.github.com>
2024-06-24 18:23:25 -0300

    chore: changed app name and fixed dark theme error

--- android/app/src/main/java/com/app/todowy/MainActivity.kt
+++ android/app/src/main/java/com/app/todowy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.todowy
+package com.app.todowy
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/app/todowy/MainApplication.kt
+++ android/app/src/main/java/com/app/todowy/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.todowy
+package com.app.todowy
 
 import android.app.Application
 import com.facebook.react.PackageList
